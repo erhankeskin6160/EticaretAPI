5d750c5d5c6cc80810dba90c6053f60a21f878bf
Erhan Keskin <erhankeskin0661@gmail.com>
2025-12-22 23:23:35 +0300

    Frontend Mimarisi Tasarlandı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui/components/components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { HomeModule } from './home/home.module';
+import { BasketsModule } from './baskets/baskets.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    HomeModule,
+    BasketsModule
+  ],
+  exports: [
+    ProductsModule,
+    HomeModule,
+    BasketsModule
+  ]
+})
+export class ComponentsModule { }
