594e37746f0bfde7a58ff2209c1028321da11e94
Erhan Keskin <erhankeskin0661@gmail.com>
2026-01-09 23:20:52 +0300

    CQrs Politikası Ayarlandı

--- Presentation/EticaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/EticaretAPI.API/Controllers/ProductsController.cs
@@ -19,29 +19,24 @@ namespace EticaretAPI.API.Controllers
             this.productReadRepository = productReadRepository;
             this.orderWriteRepository = orderWriteRepository;
         }
+        //[HttpGet]
+        //public async Task Get()
+        //{
+        //    await productWriteRepository.AddAsync(new()
+        //    {
+        //        Name = "Product 1",
+        //        Price = 100,
+        //        Stock = 10,
+        //        CreatedDate = DateTime.UtcNow
+        //    });
+        //    await productWriteRepository.SaveAsync();
+        //}
+
         [HttpGet]
-        public async Task Get()
+        public async Task<IActionResult> Get()
         {
-            await productWriteRepository.AddAsync(new()
-            {
-                Name = "Product 1",
-                Price = 100,
-                Stock = 10,
-                CreatedDate = DateTime.UtcNow
-            });
-            await productWriteRepository.SaveAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task Get(string id)
-        {
-
-            //Product product = await  productReadRepository.GetByIdAsync(id);
-            // return Ok(product);
-            var customerId = Guid.NewGuid();
-            await customerWriteRepository.AddAsync(new() { Id = customerId, Name = "Mesfd" });
-            await orderWriteRepository.AddAsync(new() { Description = "sad", Adress = "dfdsff sadsad",CustomerId= customerId });
-            await orderWriteRepository.SaveAsync();
+            return Ok("Merhaba");
+            
         }
 
     }
--- Presentation/EticaretAPI.API/Program.cs
+++ Presentation/EticaretAPI.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistenceServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200/")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
